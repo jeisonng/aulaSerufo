5f639bff905254924f0012c575ab7732362584da
jeison guimaraes azevedo <jeison.azevedo.ads@gmail.com>
2018-08-29 20:21:38 -0300

    Continuação

--- app/src/main/java/com/example/jeison/aulaserufo/MainActivity.java
+++ app/src/main/java/com/example/jeison/aulaserufo/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.jeison.aulaserufo;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.security.Principal;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnOK;
@@ -16,12 +19,23 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnOK = (Button)findViewById(R.id.botao);
-        edtText= (EditText)findViewById(R.id.texto);
+        btnOK = findViewById(R.id.botao);
+        edtText= findViewById(R.id.texto);
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder dig = new AlertDialog.Builder();
+                AlertDialog.Builder dig = new AlertDialog.Builder(MainActivity.this);
+                dig.setTitle("teste");
+                dig.setMessage(edtText.getText().toString());
+                dig.setCancelable(false);
+                dig.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).show();
+
+
             }
         });
     }
