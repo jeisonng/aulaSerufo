3fba3174e9a390fc4e4b1d9f34014e9bbec40a2d
jeison guimaraes azevedo <jeison.azevedo.ads@gmail.com>
2018-09-04 20:17:54 -0300

    Continuação

--- app/src/main/java/com/example/jeison/aulaserufo/MainActivity.java
+++ app/src/main/java/com/example/jeison/aulaserufo/MainActivity.java
@@ -7,18 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.security.Principal;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnOK;
     private EditText edtText;
+    private int[] imagem={R.drawable.crianca,R.drawable.adolescente,R.drawable.adulto,R.drawable.velho};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView teste= findViewById(R.id.imageView);
         btnOK = findViewById(R.id.botao);
         edtText= findViewById(R.id.texto);
         btnOK.setOnClickListener(new View.OnClickListener() {
@@ -28,28 +31,37 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     i = Integer.parseInt(edtText.getText().toString());
 
-                    AlertDialog.Builder dig = new AlertDialog.Builder(MainActivity.this);
+                 //  AlertDialog.Builder dig = new AlertDialog.Builder(MainActivity.this);
 
                     if (i <= 12) {
-                        dig.setMessage("Criança");
+                      //  dig.setIcon(R.drawable.crianca);
+                       // dig.setMessage("Criança");
+                        teste.setImageResource(imagem[0]);
                     } else if (i > 12 && i < 18) {
-                        dig.setMessage("Adolescente");
+                      //  dig.setIcon(R.drawable.adolescente);
+                        teste.setImageResource(imagem[1]);
+
+                     //   dig.setMessage("Adolescente");
                     } else if (i >= 18 && i < 60) {
-                        dig.setMessage("Adulto");
+                     //   dig.setIcon(R.drawable.adulto);
+                     //   dig.setMessage("Adulto");
+                        teste.setImageResource(imagem[2]);
                     } else {
-                        dig.setMessage("Idoso");
+                     //   dig.setIcon(R.drawable.velho);
+                     //   dig.setMessage("Velho");
+                        teste.setImageResource(imagem[3]);
 
                     }
-                    dig.setTitle("Idade");
-                    dig.setCancelable(false);
-                    dig.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
+                   // dig.setTitle("Idade");
+                   // dig.setCancelable(false);
+                   // dig.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                   //     @Override
+                   //     public void onClick(DialogInterface dialog, int which) {
 
-                        }
-                    }).show();
+                  //      }
+                   // }).show();
                 }catch (Exception e){
-                    AlertDialog.Builder dig = new AlertDialog.Builder(MainActivity.this);
+                  /*  AlertDialog.Builder dig = new AlertDialog.Builder(MainActivity.this);
                     dig.setTitle("Idade");
                     dig.setMessage("Digite um valor valido!");
                     dig.setCancelable(false);
@@ -58,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                         public void onClick(DialogInterface dialog, int which) {
 
                         }
-                    }).show();
+                    }).show();*/
                 }
 
 
